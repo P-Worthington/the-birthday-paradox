d4952c9a7d44f8c7420be69f55e5d540480e908d
Paul Worthington <pworthington17@gmail.com>
2024-07-16 13:23:16 +0000

    complete

--- bd.py
+++ bd.py
@@ -1,72 +1,99 @@
-import random 
-
-from const import dateTable
-
-def welcome ():
-    print("Welcome to Birthday Paradox")
-
-def numberPeople ():
-    while True:
-        print("How many people? (Min 2 Max 100)")
-        x = input("\n")
-        try: 
-            y = int(x)
-            if y >= 2 and y <= 100:
-                return y
-                break
-            else:
-                print("Please provide valid input")
-        except:
-            print("Please provide valid input")
-
-
-def generateBirthdays (x):
+import datetime, random
+ 
+def getBirthdays(numberOfBirthdays):
+    """Returns a list of number random date objects for birthdays."""
     birthdays = []
-    i = 0 
-    while i < x:
-        date = random.randint(1, 365)
-        birthdays.append(date)
-        i += 1
+    for i in range(numberOfBirthdays):
+        # The year is unimportant for our simulation, as long as all
+        # birthdays have the same year.
+        startOfYear = datetime.date(2001, 1, 1)
+
+        # Get a random day into the year:
+        randomNumberOfDays = datetime.timedelta(random.randint(0, 364))
+        birthday = startOfYear + randomNumberOfDays
+        birthdays.append(birthday)
     return birthdays
 
-def duplicateAssessment(birthdays, x):
-    duplicates = []
-    for date in birthdays:
-        if birthdays.count(date) > 1:
-            duplicates.append(date)
+ 
+def getMatch(birthdays):
+    """Returns the date object of a birthday that occurs more than once
+    in the birthdays list."""
+    if len(birthdays) == len(set(birthdays)):
+        return None  # All birthdays are unique, so return None.
 
-    try:
-        length = duplicates[-1]
-        if length > 0:
-            print(f'There are {length} duplicate birthdays in this gorup of {x} people')
-    except:
-        print(f"There are no duplicate birthdays in this gorup of {x} people")
+   # Compare each birthday to every other birthday:
+    for a, birthdayA in enumerate(birthdays):
+        for b, birthdayB in enumerate(birthdays[a + 1 :]):
+            if birthdayA == birthdayB:
+                return birthdayA  # Return the matching birthday.
+  
+ # Display the intro:
+print('''Birthday Paradox, by Al Sweigart al@inventwithpython.com
+ The birthday paradox shows us that in a group of N people, the odds
+ that two of them have matching birthdays is surprisingly large.
+ This program does a Monte Carlo simulation (that is, repeated random
+ simulations) to explore this concept.
+ 
+ (It's not actually a paradox, it's just a surprising result.)
+ ''')
+ 
+# Set up a tuple of month names in order:
+MONTHS = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+         'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
 
-def largeIteration(x):
-    positive = 0
-    i = 0
-    while i < 100000:
-        birthdays = generateBirthdays(x)
-        for date in birthdays:
-            if birthdays.count(date) > 1:
-                positive += 1
-        i += 1
-    
-    percentage = percentageMaker(positive, 100000)
-    print(f'In a group of {x} people 100_000 times there are prepeat birthdays {percentage}% number of times')
+while True:  # Keep asking until the user enters a valid amount.
+    print('How many birthdays shall I generate? (Max 100)')
+    response = input('> ')
+    if response.isdecimal() and (0 < int(response) <= 100):
+        numBDays = int(response)
+        break  # User has entered a valid amount.
+print()
 
-def percentageMaker (result, total):
-    return (result / total) *100
+# Generate and display the birthdays:
+print('Here are', numBDays, 'birthdays:')
+birthdays = getBirthdays(numBDays)
+for i, birthday in enumerate(birthdays):
+    if i != 0:
+        # Display a comma for each birthday after the first birthday.
+       print(', ', end='')
+    monthName = MONTHS[birthday.month - 1]
+    dateText = '{} {}'.format(monthName, birthday.day)
+    print(dateText, end='')
+print()
+print()
 
-def main ():
-    welcome()
-    x = numberPeople()
-    birthdays = generateBirthdays(x)
-    duplicateAssessment(birthdays, x)
-    print(f"Performing 100_000 iterations of {x} people and providing the percentage of times a dupliate was found")
-    largeIteration(x)
+# Determine if there are two birthdays that match.
+match = getMatch(birthdays)
 
-main()
+# Display the results:
+print('In this simulation, ', end='')
+if match != None:
+    monthName = MONTHS[match.month - 1]
+    dateText = '{} {}'.format(monthName, match.day)
+    print('multiple people have a birthday on', dateText)
+else:
+    print('there are no matching birthdays.')
+print()
 
+# Run through 100,000 simulations:
+print('Generating', numBDays, 'random birthdays 100,000 times...')
+input('Press Enter to begin...')
 
+print('Let\'s run another 100,000 simulations.')
+simMatch = 0  # How many simulations had matching birthdays in them.
+for i in range(100000):
+    # Report on the progress every 10,000 simulations:
+    if i % 10000 == 0:
+        print(i, 'simulations run...')
+    birthdays = getBirthdays(numBDays)
+    if getMatch(birthdays) != None:
+        simMatch = simMatch + 1
+print('100,000 simulations run.')
 
+# Display simulation results:
+probability = round(simMatch / 100000 * 100, 2)
+print('Out of 100,000 simulations of', numBDays, 'people, there was a')
+print('matching birthday in that group', simMatch, 'times. This means')
+print('that', numBDays, 'people have a', probability, '% chance of')
+print('having a matching birthday in their group.')
+print('That\'s probably more than you would think!')
